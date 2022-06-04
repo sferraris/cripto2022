3012706a1c02f8101a1be5d286c52788a775c42d
sferraris <sferraris@itba.edu.ar>
2022-06-04 19:36:02 -0300

    Agrego los bmpreaders

---
+++ bpmreader.h
@@ -0,0 +1,12 @@
+//
+// Created by santi on 6/2/2022.
+// http://paulbourke.net/dataformats/bmp/parse.c
+//
+
+#ifndef CRIPTO2022_BPMREADER_H
+#define CRIPTO2022_BPMREADER_H
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+#endif //CRIPTO2022_BPMREADER_H
